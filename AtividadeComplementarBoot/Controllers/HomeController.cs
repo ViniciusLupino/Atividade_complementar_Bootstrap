384993c213a5dbf7832e28e359ff0851992abe3a
Vinícius de Mattos Lupino <viniciusdemattoslupino@gmail.com>
2023-10-18 16:04:53 -0300

    Inicial

---
+++ AtividadeComplementarBoot/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using AtividadeComplementarBoot.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AtividadeComplementarBoot.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Serjao()
+        {
+            return View();
+        }
+
+        public IActionResult Creditos()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
